e13e21b750273e429cbbd123b81107cdd0920758
rafaelcassiolla <rafaelbernardo12790@gmail.com>
2026-04-14 21:09:07 -0300

    iniciando-javascript

---
+++ praticas-aulas/java-script/aula1-js/script.js
@@ -0,0 +1,10 @@
+
+var nome = prompt("Qual o seu nome?");
+
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1> Bem vindo " + nome + "</h1>")
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />")
