029016ca6342a3d80c6b55492333281b3e321c16
rafaelcassiolla <rafaelbernardo12790@gmail.com>
2026-05-07 22:40:42 -0300

    operadores js

--- praticas-aulas/java-script/testes-02/script.js
+++ praticas-aulas/java-script/testes-02/script.js
@@ -1,34 +1,17 @@
-//
-
-// let pessoa = {
-//     nome: "João",
-//     sobrenome: "Silva",
-//     empresa: "Tech Solutions",
-//     cargo: "Desenvolvedor",
-// };
-
-// console.log(pessoa.nome); // Acessando a propriedade 'nome'
-// console.log(pessoa.sobrenome); // Acessando a propriedade 'sobrenome'
 
 
+// function convidados (...nomes){
+//     console.log("Lista de convidados:");
+//     console.log(nomes);
+// }
 
-// const { nome, sobrenome, empresa, cargo } = pessoa; // Desestruturação do objeto 'pessoa'
+// convidados("Maria", "João", "Ana", "Carlos");
 
-// console.log(nome);
-// console.log(sobrenome);
+function sorteador(...numeros) {
+    console.log(numeros);
 
-// console.log(empresa);
-// console.log(cargo);
+    const numeroSorteado = Math.floor(Math.random() * numeros.length);
+    console.log(numeros[numeroSorteado]);
+}
 
-
-let nomes = ["João", "Maria", "Pedro"];
-
-// let { 0:primeira} = nomes;
-
-// console.log(primeira); // Acessando o elemento 'João' do array 'nomes' usando desestruturação
-
-let [primeiroNome, segundoNome, terceiroNome] = nomes;
-
-console.log(primeiroNome); // Acessando o elemento 'João' do array 'nomes' usando desestruturação
-console.log(segundoNome); // Acessando o elemento 'Maria' do array 'nomes' usando desestruturação
-console.log(terceiroNome); // Acessando o elemento 'Pedro' do array 'nomes' usando desestruturação
+sorteador(5, 10, 15, 20);
